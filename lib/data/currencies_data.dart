2dee42a859b054a8614b104e3bb43d044ad79477
deepak-1401 <g.deepak1401@gmail.com>
2026-06-05 01:39:44 +0530

    feat(settings): Completed Currency UI Section

---
+++ lib/data/currencies_data.dart
@@ -0,0 +1,85 @@
+class CurrencyData {
+  static const List<
+    Map<
+      String,
+      String
+    >
+  >
+  currencies = [
+    {
+      "code": "INR",
+      "symbol": "₹",
+      "name": "Indian Rupee",
+    },
+    {
+      "code": "USD",
+      "symbol": "\$",
+      "name": "US Dollar",
+    },
+    {
+      "code": "EUR",
+      "symbol": "€",
+      "name": "Euro",
+    },
+    {
+      "code": "GBP",
+      "symbol": "£",
+      "name": "British Pound",
+    },
+    {
+      "code": "JPY",
+      "symbol": "¥",
+      "name": "Japanese Yen",
+    },
+    {
+      "code": "AUD",
+      "symbol": "A\$",
+      "name": "Australian Dollar",
+    },
+    {
+      "code": "CAD",
+      "symbol": "C\$",
+      "name": "Canadian Dollar",
+    },
+    {
+      "code": "CHF",
+      "symbol": "CHF",
+      "name": "Swiss Franc",
+    },
+    {
+      "code": "SGD",
+      "symbol": "S\$",
+      "name": "Singapore Dollar",
+    },
+    {
+      "code": "AED",
+      "symbol": "د.إ",
+      "name": "UAE Dirham",
+    },
+    {
+      "code": "CNY",
+      "symbol": "¥",
+      "name": "Chinese Yuan",
+    },
+    {
+      "code": "KRW",
+      "symbol": "₩",
+      "name": "South Korean Won",
+    },
+    {
+      "code": "MYR",
+      "symbol": "RM",
+      "name": "Malaysian Ringgit",
+    },
+    {
+      "code": "THB",
+      "symbol": "฿",
+      "name": "Thai Baht",
+    },
+    {
+      "code": "ZAR",
+      "symbol": "R",
+      "name": "South African Rand",
+    },
+  ];
+}
--- lib/screens/setting/setting_section_UI/currency_page.dart
+++ lib/screens/setting/setting_section_UI/currency_page.dart
@@ -1,3 +1,4 @@
+import 'package:budget_manager/data/currencies_data.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyPage
@@ -23,15 +24,82 @@ class _CurrencyPageState
   Widget build(
     BuildContext context,
   ) {
-    return const AlertDialog(
-      backgroundColor: Color(
+    return AlertDialog(
+      backgroundColor: const Color(
         0xFF161D47,
       ),
-      title: Text(
+      title: const Text(
         'Currency',
       ),
-      content: Column(
-        children: [],
+      content: SizedBox(
+        width: double.maxFinite,
+        height: 350,
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color(
+              0xFF0F1330,
+            ),
+            borderRadius: BorderRadius.circular(
+              10,
+            ),
+          ),
+          child: ListView.separated(
+            itemCount: CurrencyData.currencies.length,
+            itemBuilder:
+                (
+                  context,
+                  index,
+                ) {
+                  final currency = CurrencyData.currencies[index];
+
+                  return Material(
+                    color: Colors.transparent,
+                    child: ListTile(
+                      leading: Text(
+                        currency['symbol']!,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+
+                      title: Text(
+                        currency['name']!,
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      subtitle: Text(
+                        currency['code']!,
+                        style: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.pop(
+                          context,
+                          currency,
+                        );
+                      },
+                    ),
+                  );
+                },
+            separatorBuilder:
+                (
+                  BuildContext context,
+                  int index,
+                ) {
+                  return const Divider(
+                    color: Color(
+                      0xFF252B5C,
+                    ),
+                    thickness: 2.5,
+                    indent: 20,
+                    endIndent: 20,
+                  );
+                },
+          ),
+        ),
       ),
     );
   }
