b7222a11d0df0801dd390c030a5e993d34a10ba7
deepak-1401 <g.deepak1401@gmail.com>
2026-06-04 16:23:40 +0530

    feat(settings): begin work on Settings Page UI

--- lib/screens/setting/settings.dart
+++ lib/screens/setting/settings.dart
@@ -1,10 +1,422 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class Setting extends StatelessWidget {
-  const Setting({super.key});
+class Setting
+    extends
+        StatefulWidget {
+  const Setting({
+    super.key,
+  });
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  State<
+    Setting
+  >
+  createState() => _SettingState();
+}
+
+class _SettingState
+    extends
+        State<
+          Setting
+        > {
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(
+                8.0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Setting',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+            Container(
+              width:
+                  MediaQuery.of(
+                    context,
+                  ).size.width *
+                  0.9,
+              padding: const EdgeInsets.all(
+                16,
+              ),
+              decoration: BoxDecoration(
+                color: const Color(
+                  0xFF161D47,
+                ),
+                borderRadius: BorderRadius.circular(
+                  20,
+                ),
+              ),
+              child: Row(
+                children: [
+                  // Avatar
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: const Color(
+                        0xFF9B4EFF,
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        30,
+                      ),
+                    ),
+                    child: const Icon(
+                      Icons.person,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                  ),
+
+                  const SizedBox(
+                    width: 15,
+                  ),
+
+                  // Name & Email
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Deepak",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+
+                        SizedBox(
+                          height: 4,
+                        ),
+
+                        Text(
+                          "deepak@gmail.com",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // Edit Button
+                  Container(
+                    padding: const EdgeInsets.all(
+                      8,
+                    ),
+                    decoration: BoxDecoration(
+                      color: const Color(
+                        0xFF9B4EFF,
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                    ),
+
+                    child: const Icon(
+                      Icons.arrow_forward_ios_outlined,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              width:
+                  MediaQuery.of(
+                    context,
+                  ).size.width *
+                  0.9,
+              padding: const EdgeInsets.all(
+                16,
+              ),
+              decoration: BoxDecoration(
+                color: const Color(
+                  0xFF161D47,
+                ),
+                borderRadius: BorderRadius.circular(
+                  20,
+                ),
+              ),
+
+              child: Row(
+                children: [
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: const Color(
+                        0xFF9B4EFF,
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        30,
+                      ),
+                    ),
+                    child: const Icon(
+                      CupertinoIcons.sun_max_fill,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Theme",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+
+                        Text(
+                          "Dark",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(
+                      8,
+                    ),
+                    decoration: BoxDecoration(
+                      color: const Color(
+                        0xFF9B4EFF,
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                    ),
+
+                    child: const Icon(
+                      Icons.arrow_forward_ios_outlined,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              width:
+                  MediaQuery.of(
+                    context,
+                  ).size.width *
+                  0.9,
+              padding: const EdgeInsets.all(
+                16,
+              ),
+              decoration: BoxDecoration(
+                color: const Color(
+                  0xFF161D47,
+                ),
+                borderRadius: BorderRadius.circular(
+                  20,
+                ),
+              ),
+
+              child: Row(
+                children: [
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: const Color(
+                        0xFF9B4EFF,
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        30,
+                      ),
+                    ),
+                    child: const Icon(
+                      Icons.currency_rupee_rounded,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Currency",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+
+                        Text(
+                          "Indian Rupee (₹)",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(
+                      8,
+                    ),
+                    decoration: BoxDecoration(
+                      color: const Color(
+                        0xFF9B4EFF,
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                    ),
+
+                    child: const Icon(
+                      Icons.arrow_forward_ios_outlined,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              width:
+                  MediaQuery.of(
+                    context,
+                  ).size.width *
+                  0.9,
+              padding: const EdgeInsets.all(
+                16,
+              ),
+              decoration: BoxDecoration(
+                color: const Color(
+                  0xFF161D47,
+                ),
+                borderRadius: BorderRadius.circular(
+                  20,
+                ),
+              ),
+
+              child: Row(
+                children: [
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: const Color(
+                        0xFF9B4EFF,
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        30,
+                      ),
+                    ),
+                    child: const Icon(
+                      Icons.lock_outline,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Change Password",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+
+                        Text(
+                          "Update your password",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(
+                      8,
+                    ),
+                    decoration: BoxDecoration(
+                      color: const Color(
+                        0xFF9B4EFF,
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                    ),
+
+                    child: const Icon(
+                      Icons.arrow_forward_ios_outlined,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
